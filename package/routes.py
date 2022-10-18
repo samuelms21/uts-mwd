64b5c2b4ab83fddae64693a51484ad3a1e7623ab
Samuel M. Setiawan <70130148+samuelms21@users.noreply.github.com>
2022-10-18 19:37:55 +0700

    Update routes.py

--- package/routes.py
+++ package/routes.py
@@ -208,4 +208,30 @@ def manager_cust():
             
     return render_template('manager_cust.html', title=title, all_cust_o=all_cust_o)
 
+@app.route("/update_cust", methods=["POST"])
+def update_cust():
+    cust_id = request.form.get("cust_id")
 
+    newname = request.form.get("newname")
+
+    newaddress = request.form.get("newaddress")
+
+    newphone = request.form.get("newphone")
+
+    customer = Customer.query.filter_by(cust_id=cust_id).first()
+    customer.name = newname
+    customer.address = newaddress
+    customer.phone = newphone
+    db.session.commit()
+
+
+    return redirect(url_for('manager_cust'))
+
+
+@app.route("/delete_cust", methods=["POST"])
+def delete_cust():
+    cust_id = request.form.get("cust_id")
+    customer = Customer.query.filter_by(cust_id=cust_id).first()
+    db.session.delete(customer)
+    db.session.commit()
+    return redirect(url_for('manager_cust'))
