93d14f5b91613d5512cdc57d0d1e5cd19465a2ea
samuelms21 <ssamuelmarcellino@gmail.com>
2022-10-21 07:44:18 +0700

    Update mobile responsive layout and add mobile navigation menu

--- package/static/index.js
+++ package/static/index.js
@@ -1,3 +1,13 @@
-const navbar = document.querySelector("nav");
-//   const header = document.querySelector("header");
-const mobileMenu = document.querySelector("#mobileMenu");
+const navbar = document.querySelector(".nav__link");
+const menuBtn = document.querySelector("#mobileMenu");
+const mobileMenu = document.querySelector(".mobile-menu-links");
+
+menuBtn.addEventListener("click", function () {
+  mobileMenu.classList.toggle("hide");
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 768) {
+    mobileMenu.classList.add("hide");
+  }
+});
